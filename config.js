0f4043767de80403fe2f5750c07f579737575d37
Vendetta <34066247+CistusF@users.noreply.github.com>
2023-04-10 19:24:47 +0900

    Update config.js

--- config.js
+++ config.js
@@ -5,19 +5,24 @@ var SNS_List = [
     {
         link: "https://instagram.com/cistusf",
         type: "instagram",
-        alt: "My public IG"
+        bio: "My public IG"
     },
     {
         link: "https://instagram.com/v_cistus",
         type: "instagram",
-        alt: "My private IG"
+        bio: "My private IG"
     },
     {
         link: "https://youtube.com/@cistusf",
-        type: "youtube"
+        type: "youtube",
+        bio: "My Music Room"
     },
     {
         link: "https://steamcommunity.com/id/cistusgame",
         type: "steam"
+    },
+    {
+        link: "https://cistusf.vercel.app/",
+        bio: "My Portfolio"
     }
 ];
